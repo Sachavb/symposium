d76f7b5cef0249c4632fa30b3f524538d0262a2a
Jesús García <ctw@ctwhome.com>
2022-05-31 14:52:33 +0200

    Add partners logos

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Billboard from "../lib/components/Billboard.svelte";
+  import Participants from "../lib/components/Participants.svelte";
   import Agenda from "$lib/Agenda.svelte";
 </script>
 
@@ -13,19 +14,20 @@
 
 <Agenda/>
 
-<div class="prose container mx-auto my-20 ">
-  <h2>Participants</h2>
-  <ul>
-    <li>Dutch Research Council, NWO (the Netherlands)</li>
-    <li>Agence nationale de la recherche, ANR (France)</li>
-    <li>Deutsche Forschungsgemeinschaft, DFG (Germany)</li>
-    <li>UK Research & Innovation, UKRI (UK)</li>
-    <li>Fonds zur Förderung der wissenschaftlichen Forschung, FWF (Austria)</li>
-    <li>Forskningsråd, RCN (Norway)</li>
-    <li>Research Foundation Flanders, FWO (Flanders)</li>
-    <li>Romania Executive Agency for Higher Education, Research, Development and Innovation Funding of Romania, UEFISCDI (Romania)</li>
-    <li>Fundação para a Ciência e Tecnologia, FCT (Portugal)</li>
-  </ul>
-</div>
+
+<Participants/>
+<!--<div class="prose container mx-auto my-20 ">-->
+<!--  <ul>-->
+<!--    <li>Dutch Research Council, NWO (the Netherlands)</li>-->
+<!--    <li>Agence nationale de la recherche, ANR (France)</li>-->
+<!--    <li>Deutsche Forschungsgemeinschaft, DFG (Germany)</li>-->
+<!--    <li>UK Research & Innovation, UKRI (UK)</li>-->
+<!--    <li>Fonds zur Förderung der wissenschaftlichen Forschung, FWF (Austria)</li>-->
+<!--    <li>Forskningsråd, RCN (Norway)</li>-->
+<!--    <li>Research Foundation Flanders, FWO (Flanders)</li>-->
+<!--    <li>Romania Executive Agency for Higher Education, Research, Development and Innovation Funding of Romania, UEFISCDI (Romania)</li>-->
+<!--    <li>Fundação para a Ciência e Tecnologia, FCT (Portugal)</li>-->
+<!--  </ul>-->
+<!--</div>-->
 
 
