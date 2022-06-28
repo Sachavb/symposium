64558fde5c0c1bdf3e6da642e6f07e30f28167f2
Jesús García <ctw@ctwhome.com>
2022-06-28 18:57:29 +0200

    Update organizers images with cards

--- src/content/organizers.md
+++ src/content/organizers.md
@@ -3,6 +3,42 @@ title1: About the organizers
 image1: eScience-4.jpg
 
 title2: Advisory committee
+advisory:
+  - name: Michelle Barker 
+    desc: director Research Software Alliance
+    image: https://pbs.twimg.com/profile_images/1288053506427875328/YRBP5i5B_400x400.jpg
+  - name: Neil Chue Hong 
+    desc: Software Sustainability Institute, University of Edinburgh, UK
+    image: https://pbs.twimg.com/profile_images/1089864582/neil_profile_filmfest_400x400.jpg
+  - name: Maria Cruz 
+    desc: NWO Dutch Research Council
+    image: https://www.nwo.nl/sites/nwo/files/styles/contact/public/gallery/Marques%20de%20Barros%20Cruz%2C%20Maria.jpg
+  - name: Daniel S. Katz 
+    desc: National Center for Supercomputing Applications (NCSA), University of Illinois, USA
+    image: https://danielskatz.org/gifs/dsk.png
+  - name: Joris van Eijnatten
+    desc: CEO The Netherlands eScience Center
+    image: https://www.esciencecenter.nl/wp-content/uploads/2020/02/J.v.Eijnatten2-1-300x300.jpg
 
 title3: Organizing committee
+organizers:
+  - name: Valentina Azzarà
+    desc: Section Head, Social Sciences & Humanities
+    image: https://www.esciencecenter.nl/wp-content/uploads/2021/10/ValentinaAzzara_picture-300x300.jpg
+  - name: Tom Bakker
+    desc: Policy Advisor
+    image: https://www.esciencecenter.nl/wp-content/uploads/2020/02/IMG_0376-1-300x300.jpg
+  - name: Sacha van Breugel
+    desc: Event Manager
+    image: https://www.esciencecenter.nl/wp-content/uploads/2020/02/IMG_1494-1-300x300.jpg
+  - name: Carlos Martinez Ortiz
+    desc: Community Manager, Natural Sciences & Engineering
+    image: https://www.esciencecenter.nl/wp-content/uploads/2020/02/IMG_0369-1-300x300.jpg
+  - name: Veronica Pang
+    desc: Communications Advisor
+    image: https://www.esciencecenter.nl/wp-content/uploads/2021/07/Pang-Veronica--300x300.png
+  - name: Joris van Eijnatten
+    desc: CEO The Netherlands eScience Center
+    image: https://www.esciencecenter.nl/wp-content/uploads/2020/02/J.v.Eijnatten2-1-300x300.jpg
+
 ---
--- src/routes/organizers.svelte
+++ src/routes/organizers.svelte
@@ -17,13 +17,15 @@
         <p class="">
           The <a target="_blank" href="https://www.researchsoft.org/">Research Software Alliance (ReSA)</a> is an international community of professionals whose
           mission is to “bring research software communities together
-          to collaborate on the advancement of the research software ecosystem.” Together, they ensure that “research software and those who develop and maintain
+          to collaborate on the advancement of the research software ecosystem.” Together, they ensure that “research software and those who develop and
+          maintain
           it are recognized and valued as fundamental and vital to research worldwide.”
         </p>
         <p class="mt-8">
           The <a target="_blank" href="https://www.esciencecenter.nl/">Netherlands eScience Center</a> is a research organization that since 2012 has been
           uniquely positioned as an independent foundation, financed by the Dutch Research Council (NWO) and the Dutch organization for IT in
-          education and research (SURF). It is the national centre with the digital skills to create innovative software solutions in academic software. The core
+          education and research (SURF). It is the national centre with the digital skills to create innovative software solutions in academic software. The
+          core
           activities of the eScience Center are carried out by Research Software Engineers (RSEs) who are high-end, university-level specialists who do two
           things: they do research using digital technologies and methodologies, and they engineer software.
         </p>
@@ -38,40 +40,40 @@
   <div class="font-bold text-2xl">
     {content.title2}
   </div>
-  <div>
-    <ul>
-      <li>
-        Michelle Barker (director Research Software Alliance)
-      </li>
-      <li>
-        Neil Chue Hong (Software Sustainability Institute, University of Edinburgh, UK)
-      </li>
-      <li>
-       Maria Cruz (NWO Dutch Research Council)
-      </li>
-      <li>
-        Daniel S. Katz (National Center for Supercomputing Applications (NCSA), University of Illinois, USA)
-      </li>
-      <li>
-        Joris van Eijnatten (Netherlands eScience Center)
-      </li>
-    </ul>
-    <br>
-  </div>
 
-   <div class="font-bold text-2xl">
+  <section class="container mx-auto flex flex-wrap pb-10">
+    {#each content.advisory as member}
+      <div class="p-4 w-full sm:w-1/2 lg:w-1/3">
+        <div class="flex flex-col h-full p-4 items-center rounded shadow bg-white">
+          <img class="rounded-full object-cover w-[150px] h-[150px]" alt={member.name} src={member.image}>
+          <div class="w-full lg:pl-6 p-4">
+            <h4 class="mb-2 text-2xl font-bold font-heading">
+              { member.name }
+            </h4>
+            <p class="mb-4 text-gray-700 leading-loose">{ member.desc }</p>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </section>
+
+
+  <div class="font-bold text-2xl">
     {content.title3} (Netherlands eScience Center)
   </div>
-  <div>
-     <ul>
-        <li>Valentina Azzarà</li>
-        <li>Tom Bakker</li>
-        <li>Sacha van Breugel</li>
-        <li>Carlos Martinez Ortiz</li>
-        <li>Veronica Pang</li>
-        <li>Joris van Eijnatten</li>
-     </ul>
-    <br>
-  </div>
-
+  <section class="container mx-auto flex flex-wrap pb-10">
+    {#each content.organizers as member}
+      <div class="p-4 w-full sm:w-1/2 lg:w-1/3">
+        <div class="flex flex-col h-full p-4 items-center rounded shadow bg-white">
+          <img class="rounded-full object-cover w-[150px] h-[150px]" alt={member.name} src={member.image}>
+          <div class="w-full lg:pl-6 p-4">
+            <h4 class="mb-2 text-2xl font-bold font-heading">
+              { member.name }
+            </h4>
+            <p class="mb-4 text-gray-700 leading-loose">{ member.desc }</p>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </section>
 </div>
